a3ca2cce138128d6f5ff63d7a6d400e110bb3b5c
hichaoban <dmmvjp@gmail.com>
2025-10-28 10:34:04 +0700

    Update keyStore.js

--- utils/keyStore.js
+++ utils/keyStore.js
@@ -1,1 +1,27 @@
+// utils/keyStore.js
+const STORAGE_KEY = "gemini_api_key";
+
+export function getApiKey() {
+  try {
+    const url = new URL(window.location.href);
+    const k = url.searchParams.get("key");
+    if (k) {
+      localStorage.setItem(STORAGE_KEY, k);
+      url.searchParams.delete("key");
+      window.history.replaceState({}, "", url.toString());
+      return k;
+    }
+    return localStorage.getItem(STORAGE_KEY);
+  } catch {
+    return null;
+  }
+}
+
+export function setApiKey(key) {
+  localStorage.setItem(STORAGE_KEY, key);
+}
+
+export function clearApiKey() {
+  localStorage.removeItem(STORAGE_KEY);
+}
 
